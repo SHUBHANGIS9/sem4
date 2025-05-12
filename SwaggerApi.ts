8dbae72d3f2aa7a6795faff0872e16cd44ef26de
SHUBHANGIS9 <shubhangisharma2001@gmail.com>
2025-05-12 19:30:56 +0530

    Delete SwaggerApi.ts

--- SwaggerApi.ts
+++
@@ -1,7 +0,0 @@
-// src/SwaggerApi.ts
-import axios from 'axios';
-
-export const fetchSwaggerDefinition = async (url: string) => {
-  const response = await axios.get(url);
-  return response.data;
-};
